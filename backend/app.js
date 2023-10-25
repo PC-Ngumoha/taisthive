4544eda246b9c6f832e03c8ed350617d1879985c
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-25 06:50:46 +0100

    Setup: setup basic express server

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * app.js 
+ * 
+ * Contains main API code.
+ */
+const express = require('express');
+const app = express()
+
+
+app.listen(3000, () => {
+  console.log('Server up and running at port 3000')
+});
