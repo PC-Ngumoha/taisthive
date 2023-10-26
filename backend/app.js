16da0b8918a49a941b529c8f266fe509b1f9c290
PC-Ngumoha <pcngumoha@gmail.com>
2023-10-26 18:07:31 +0100

    Update: Attempt at implementing base of the API CRUD operations

--- backend/app.js
+++ backend/app.js
@@ -123,8 +123,149 @@ db.sync()
   console.log(`Unable to create table: ${err}`);
 });
 
-app.get('/', (req, res) => {
-  res.status(200).send({Greetings: 'Hello User'});
+// app.get('/', (req, res) => {
+//   res.status(200).send({Greetings: 'Hello User'});
+// });
+
+/**
+ * What do I want:
+ * 
+ * For users:
+ * POST /user -> create a new user
+ * 
+ * PUT /user/:id -> update a particular user
+ * 
+ * DELETE /user/:id -> Delete a particular user
+ * 
+ * For recipes:
+ * 
+ * POST /recipe/:user_id -> create a new recipe
+ * 
+ * PUT /recipe/:id -> update a particular recipe
+ * 
+ * DELETE /recipe/:id -> Delete a particular recipe
+ * 
+ * GET /recipe/:user_id -> Get all recipes created by a given user
+ * 
+ * GET /recipe/:id -> Get a particular recipe 
+ */
+
+// USER
+
+// POST /user
+app.post('/user', async (req, res) => {
+  const data = req.body;
+  try {
+    const user = await User.create(data);
+    res.status(201).send({user});
+  } catch (err) {
+    res.status(401).send({error: err.message});
+  }
+});
+
+// PUT /user/:id 
+app.put('/user/:id', async (req, res) => {
+  const data = req.body;
+  const id = req.params.id;
+  try {
+    await User.update(data, {
+      where: {
+        id
+      }
+    });
+    res.status(200).send({message: 'User account updated successfully'});
+  } catch (err) {
+    res.status(500).send({message: err.message});
+  }
+});
+
+// DELETE /user/:id
+app.delete('/user/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    await User.destroy({
+      where: {
+        id
+      }
+    });
+    res.status(200).send({message: 'User account deleted successfully'});
+  } catch (err) {
+    res.status(500).send();
+  }
+});
+
+// GET /user/:id
+app.get('/user/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const user = await User.findOne({
+      where: {
+        id
+      }
+    });
+    if (user === null) {
+      return res.status(404).send({message: "User not found"});
+    }
+    res.status(200).send({user});
+  } catch (err) {
+    res.status(500).send({error: err.message});
+  }
+});
+
+
+// RECIPE
+
+// POST /recipe/:user_id -> create a new recipe
+app.post('/recipe/:user_id', async (req, res) => {
+  const data = req.body;
+  const userId = req.params.user_id;
+  try {
+    const recipe = await Recipe.create(data);
+    const author = await User.findOne({
+      where: {
+        id: userId
+      }
+    });
+    // Connect the recipe to the user.
+    await author.addRecipe(recipe);
+    res.status(200).send({recipe});
+  } catch (err) {
+    res.status(500).send({message: err.message});
+  }
+}); 
+
+// PUT /recipe/:id -> update a particular recipe
+app.put('/recipe/:id', async (req, res) => {
+
+});
+
+// DELETE /recipe/:id -> Delete a particular recipe
+app.delete('/recipe/:id', async (req, res) => {
+
+});
+
+// GET /recipe/:user_id -> Get all recipes created by a given user
+app.get('/recipe/user/:user_id', async (req, res) => {
+  const authorId = req.params.user_id;
+  try {
+    const author = await User.findOne({
+      where: {
+        id: authorId
+      }
+    });
+    if (!author) {
+      return res.status(404).send({message: 'Author with that ID not found'});
+    }
+    const recipes = await author.getRecipes();
+    res.status(200).send({recipes});
+  } catch (err) {
+    res.status(500).send({message: 'We could not get the recipes for this user'});
+  }
+});
+
+// GET /recipe/:id -> Get a particular recipe 
+app.get('/recipe/:id', async (req, res) => {
+
 });
 
 app.listen(3000, () => {
