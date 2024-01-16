8c5bc222e94ead1e85a87a49a697a578de4edcd4
PC-Ngumoha <pcngumoha@gmail.com>
2024-01-16 19:02:15 +0100

    Update: Create definition for the 'Recipe' DB table

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """Recipe DB model definition"""
+    id = models.AutoField()
+    name = models.CharField(max_length=250)
+    description = models.TextField()
+    ingredients = models.JSONField(default=list)
+    instructions = models.JSONField(default=list)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_add=True)
