d05aff4e3ac52e38f17ede71b34faba1edaeeadb
PC-Ngumoha <pcngumoha@gmail.com>
2024-01-17 21:10:53 +0100

    Update: Implement API routes for the Create & List functionality

--- backend/recipes/tests.py
+++ backend/recipes/tests.py
@@ -1,9 +1,35 @@
+from django.urls import reverse
+from recipes.models import Recipe
+from recipes.views import (CreateListRecipeView,)
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 
 class TestCreateListRecipeView(APITestCase):
     """Contains test cases for the recipe list and create view."""
-    pass
+
+    def test_should_create_new_recipe_when_data_is_passed_correctly(self):
+        recipe_count = Recipe.objects.all().count()
+        data = {
+            "name": "Simple Meal",
+            "description": "A delicious meal cooked and garnished simply",
+            "ingredients": ["tomato", "pepper", "red oil"],
+            "instructions": ["Pour all ingredients into a pot", "stir", "eat"]
+        }
+
+        response = self.client.post(
+            reverse("recipes"), data=data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Recipe.objects.all().count(), recipe_count + 1)
+
+    def test_should_be_able_to_list_all_recipes_available(self):
+        recipe_count = Recipe.objects.all().count()
+
+        response = self.client.get(reverse("recipes"))
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIsInstance(response.data, list)
+        self.assertEqual(len(response.data), recipe_count)
 
 
 class TestRetrieveUpdateDeleteRecipeView(APITestCase):
--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -1,16 +1,29 @@
+from recipes.models import Recipe
+from recipes.serializers import RecipeSerializers
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class CreateListRecipeView(APIView):
     """Create and list recipes by user request"""
+    serializer_class = RecipeSerializers
 
     def get(self, request):
         """GET - lists all the recipes available"""
-        pass
+        recipes = Recipe.objects.all()
+        serializer = self.serializer_class(recipes, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request):
         """POST - creates & stores a new recipe"""
-        pass
+        data = request.data
+        serializer = self.serializer_class(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.validated_data,
+                            status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class RetrieveUpdateDeleteRecipeView(APIView):
