2135afb6fd6edf1a03ac6c00b3eecebed42392ac
PC-Ngumoha <pcngumoha@gmail.com>
2024-01-27 16:51:03 +0100

    Update: Started work on writing the tests for the authentication views

--- backend/authentication/tests/test_views.py
+++ backend/authentication/tests/test_views.py
@@ -1,6 +1,15 @@
+from authentication.models import User
 from rest_framework.test import APITestCase
 
 
 class TestCreateUserView(APITestCase):
     """test the user create view"""
-    pass
+
+    def test_should_create_new_user_if_all_data_provided(self):
+        pass
+
+    def test_should_not_create_new_user_if_email_not_provided(self):
+        pass
+
+    def test_should_not_create_new_user_if_password_not_provided(self):
+        pass
