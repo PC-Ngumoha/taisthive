a5fdbe19663f0e310aa639469b5c3a5dc50aab5c
PC-Ngumoha <pcngumoha@gmail.com>
2024-01-27 20:10:33 +0100

    Update: Improved the ability of the 'test_and_coverage' script to target and report on the tests of specific apps in the project.

--- backend/test_and_coverage.sh
+++ backend/test_and_coverage.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/bash
 # Contains the code to automate the process of running the tests
 # and returning a coverage report
-coverage run manage.py test recipes && coverage report -m
+# coverage run manage.py test recipes && coverage report -m
+APP_NAME="$1"
+
+if [ -z "$APP_NAME" ];
+then
+    # Runs tests for all apps in the project
+    coverage run manage.py test && coverage report -m
+else
+    # Runs tests for specific app
+    coverage run manage.py test "$APP_NAME" && coverage report -m --include=./"$APP_NAME"/*.*
+fi
