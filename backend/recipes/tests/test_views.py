1acf2d1e371bb36c1757977fef0a90519d1ce3d5
PC-Ngumoha <pcngumoha@gmail.com>
2024-01-29 21:33:49 +0100

    Update: Restructured tests for the 'recipes' app

--- backend/recipes/tests/test_views.py
+++ backend/recipes/tests/test_views.py
@@ -30,34 +30,49 @@ class TestHelperTestCase(APITestCase):
     def retrieve_new_recipe_id(self):
         return Recipe.objects.all()[0].id
 
+    def authenticate(self):
+        user_data = {
+            "email": "test@example.com",
+            "password": "testing@123#"
+        }
+        self.client.post(reverse('create_user'), data=user_data, format='json')
+        response = self.client.post(
+            reverse('token_obtain_pair'), data=user_data, format='json')
+        access_token = response.data.get('access')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {access_token}')
+
 
 class TestCreateListRecipeView(TestHelperTestCase):
     """Contains test cases for the recipe list and create view."""
 
-    def test_should_create_new_recipe_when_data_is_passed_correctly(self):
-        recipe_count = Recipe.objects.all().count()
+    def test_should_create_new_recipe_when_data_passed_correctly_and_user_authenticated(self):
+        self.authenticate()
+        initial_count = Recipe.objects.all().count()
         response = self.send_create_recipe_request()
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Recipe.objects.all().count(), recipe_count + 1)
+        self.assertEqual(Recipe.objects.all().count(), initial_count + 1)
 
-    def test_should_return_400_response_when_no_data_is_passed(self):
+    def test_should_fail_to_create_new_recipe_when_user_unauthenticated(self):
+        response = self.send_create_recipe_request()
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_should_return_400_response_when_no_data_is_passed_and_user_authenticated(self):
+        self.authenticate()
         response = self.client.post(reverse("recipes"), data={})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_should_be_able_to_list_all_recipes_available(self):
-        recipe_count = Recipe.objects.all().count()
-
+    def test_should_list_all_recipes_created_by_current_authenticated_user(self):
+        self.authenticate()
         response = self.send_list_or_retrieve_recipe_request()
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIsInstance(response.data, list)
-        self.assertEqual(len(response.data), recipe_count)
 
 
 class TestRetrieveUpdateDeleteRecipeView(TestHelperTestCase):
     """Contains test cases for the recipe retrieve, update and delete view."""
 
-    def test_should_be_able_to_retrieve_a_particular_recipe(self):
+    def test_should_be_able_to_retrieve_specific_recipe_when_user_authenticated(self):
+        self.authenticate()
         post_response = self.send_create_recipe_request()
         self.assertEqual(post_response.status_code, status.HTTP_201_CREATED)
 
@@ -68,7 +83,8 @@ class TestRetrieveUpdateDeleteRecipeView(TestHelperTestCase):
         self.assertEqual(self.recipe_payload.get("name"),
                          get_response.data.get("name"))
 
-    def test_should_be_able_to_update_a_particular_recipe(self):
+    def test_should_be_able_to_update_specific_recipe_when_user_authenticated(self):
+        self.authenticate()
         modified_payload = {
             "name": "Not So Simple Meal",
             "description": "A delicious meal cooked and garnished simply",
@@ -96,7 +112,8 @@ class TestRetrieveUpdateDeleteRecipeView(TestHelperTestCase):
         self.assertEqual(get_response.data.get(
             "name"), modified_payload.get("name"))
 
-    def test_should_return_400_response_when_no_data_is_passed(self):
+    def test_should_return_400_response_when_no_data_passed_and_user_authenticated(self):
+        self.authenticate()
         post_response = self.send_create_recipe_request()
         self.assertEqual(post_response.status_code, status.HTTP_201_CREATED)
 
@@ -109,7 +126,8 @@ class TestRetrieveUpdateDeleteRecipeView(TestHelperTestCase):
             format="json")
         self.assertEqual(put_response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_should_be_able_to_delete_a_particular_recipe(self):
+    def test_should_be_able_to_delete_specific_recipe_when_user_authenticated(self):
+        self.authenticate()
         post_response = self.send_create_recipe_request()
         self.assertEqual(post_response.status_code, status.HTTP_201_CREATED)
 
