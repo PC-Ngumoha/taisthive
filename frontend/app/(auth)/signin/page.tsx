c2e5e998ae05b3f6f75cda4b63693ac2b47ae49a
PC-Ngumoha <pcngumoha@gmail.com>
2024-02-01 16:34:32 +0100

    Update: Create Sign-in and Sign-up routes

---
+++ frontend/app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+
+const SigninPage = () => {
+  return (
+    <h1>Hey, welcome to the SignIn Page</h1>
+  )
+};
+
+export default SigninPage;
---
+++ frontend/app/(auth)/signup/page.tsx
@@ -0,0 +1,8 @@
+
+const SignupPage = () => {
+  return (
+    <h1>Hey, welcome to the SignUp Page</h1>
+  )
+};
+
+export default SignupPage;
