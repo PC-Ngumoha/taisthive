5d2e24747cc900f425ed6f6c4e468b8b894547d5
PC-Ngumoha <pcngumoha@gmail.com>
2024-02-03 20:17:05 +0100

    Update: Improved the PasswordField component so that users can toggle the visibility of the passwords they input.

--- frontend/components/custom/form-fields.tsx
+++ frontend/components/custom/form-fields.tsx
@@ -1,3 +1,5 @@
+'use client';
+import { useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -32,6 +34,8 @@ const PasswordField = ({ placeholder, password, setPassword }: {
   password: string,
   setPassword: (param: string) => void
 }) => {
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
   return (
     <div className='w-[400px] flex flex-row justify-around align-middle border-b-2 border-gray-100 px-2'>
       <FontAwesomeIcon
@@ -39,18 +43,41 @@ const PasswordField = ({ placeholder, password, setPassword }: {
         className='fa-solid fa-lock text-gray-400 self-center'
       />
       <Input
-        type='password'
+        type={isVisible ? 'text' : 'password'}
         placeholder={placeholder}
         value={password}
         onChange={(evt) => { setPassword(evt.target.value) }}
         className='w-[80%] border-0 hover:border-0 self-center py-3 h-[60px]'
       />
-      <Button variant='ghost' className='w-[20px] self-center'>
-        <FontAwesomeIcon
-          icon={faEye}
-          className='fa-solid fa-eye-slash text-gray-200'
-        />
-      </Button>
+      {isVisible ? (
+        <Button
+          variant='ghost'
+          className='w-[20px] self-center'
+          onClick={(evt) => {
+            evt.preventDefault();
+            setIsVisible(false)
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faEyeSlash}
+            className='fa-solid fa-eye-slash text-gray-300'
+          />
+        </Button>
+      ) : (
+        <Button
+          variant='ghost'
+          className='w-[20px] self-center'
+          onClick={(evt) => {
+            evt.preventDefault();
+            setIsVisible(true)
+          }}
+        >
+          <FontAwesomeIcon
+            icon={faEye}
+            className='fa-solid fa-eye text-gray-300'
+          />
+        </Button>
+      )}
     </div>
   );
 }
